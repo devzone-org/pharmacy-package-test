ac6a59cd8357df3b8d092ea75af5c7ec50c72d2c
Muhammad Talha <talha8018@gmail.com>
2021-06-13 20:37:00 +0500

    update

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -23,9 +23,26 @@
                     <!-- Profile dropdown -->
                     <div class="ml-3 relative" @click.away="dropdown=false;">
                         <div>
-                            <button @click="dropdown=!dropdown;" type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=7vUgSUO79Y&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                            <button @click="dropdown=!dropdown;" type="button"
+                                    class="max-w-xs flex items-center text-sm focus:outline-none   lg:p-2 hover:bg-gray-900"
+                                    id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                <img class="h-10 w-10 rounded-full border-2 border-gray-300 hover:border-white"
+                                     src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                                     alt="">
+                                <span class="hidden ml-3 text-left text-white  font-medium lg:block">
+                                    <span
+                                        class="sr-only">Open user menu for </span>
+                                    {{ env('APP_NAME') }} <br>
+                                    <span class="text-xs">{{ Auth::user()->name }}</span>
+                                </span>
+                                <svg class="hidden flex-shrink-0 ml-2 h-5 w-5 text-gray-400 lg:block"
+                                     x-description="Heroicon name: solid/chevron-down"
+                                     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                                     aria-hidden="true">
+                                    <path fill-rule="evenodd"
+                                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                          clip-rule="evenodd"></path>
+                                </svg>
                             </button>
                         </div>
                         <div x-show="dropdown" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="origin-top-right absolute right-0 mt-2 w-48 rounded-md z-10 shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
