2edcc1ae4be435be4c9b8d9871ce52ec69c02fdb
Ali <ali2020ahtisham@outlook.com>
2022-01-19 12:28:06 +0500

    pdf/button

--- resources/views/livewire/purchases/purchase-add.blade.php
+++ resources/views/livewire/purchases/purchase-add.blade.php
@@ -252,10 +252,10 @@
             <div class="px-4 py-3 bg-gray-50 text-right sm:px-6 border-t">
                 <button type="submit"
                         class="bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
-                    <div wire:loading>
+                    <div wire:loading wire:target="create">
                         Adding ...
                     </div>
-                    <div wire:loading.remove>
+                    <div wire:loading.remove wire:target="create">
                         Add
                     </div>
                 </button>
